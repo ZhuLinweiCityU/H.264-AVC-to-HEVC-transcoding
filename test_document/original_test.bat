78e4f32f5dec881f934a161de5e4007ce043e654
ZhuLinweiCityU <lwzhu2-c@my.cityu.edu.hk>
2017-04-24 16:43:00 +0800

    test_document

---
+++ test_document/original_test.bat
@@ -0,0 +1,2 @@
+transcode_original.exe -d decoder-basketballpass.cfg -c BasketballPass.cfg -c encoder_lowdelay_P_main.cfg -q 22 -f 41>output_original.txt
+pause
---
+++ test_document/proposed_test.bat
@@ -0,0 +1,2 @@
+transcode_proposed.exe -d decoder-basketballpass.cfg -c BasketballPass.cfg -c encoder_lowdelay_P_main.cfg -q 22 -f 41>output_proposed.txt
+pause
